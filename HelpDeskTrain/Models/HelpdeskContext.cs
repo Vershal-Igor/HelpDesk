2527dc1a358b3d2248ab07a014596f9430ec1cb2
Vershal-Igor <igorvershal@gmail.com>
2020-05-27 20:41:20 +0300

    HelpDesk project on MVC4

---
+++ HelpDeskTrain/Models/HelpdeskContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HelpDeskTrain.Models
+{
+    public class HelpdeskContext
+    {
+        //Подключение к БД к таблицам
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Request> Requests { get; set; }
+        public DbSet<Lifecycle> Lifecycles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Activ> Activs { get; set; }
+    }
+}
---
+++ HelpDeskTrain/Models/Lifecycle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HelpDeskTrain.Models
+{
+    public class Lifecycle
+    {
+        // ID 
+        public int Id { get; set; }
+        // Дата открытия
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime Opened { get; set; }
+        // Дата распределения
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Distributed { get; set; }
+        // Дата обработки
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Proccesing { get; set; }
+        // Дата проверки
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Checking { get; set; }
+        // Дата закрытия
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Closed { get; set; }
+
+        // Перечисление для статуса заявки
+        public enum RequestStatus
+        {
+            Open = 1,
+            Distributed = 2,
+            Proccesing = 3,
+            Checking = 4,
+            Closed = 5
+        }
+        // Перечисление для приоритета заявки
+        public enum RequestPriority
+        {
+            Low = 1,
+            Medium = 2,
+            High = 3,
+            Critical = 4
+        }
+    }
+}
